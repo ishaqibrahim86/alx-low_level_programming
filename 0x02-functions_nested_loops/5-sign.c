75de13a00ad58d2d18c3fd0bb0d70ac9dd01aa51
Ishaq Ibrahim Sadiq <ishaqibrahim86@gmail.com>
2022-09-13 18:56:37 -0700

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,14 +3,10 @@
 
 
 /**
-
- * print_sign - print sign of number
-
- * @n: number to check
-
- * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
-
- */
+* print_sign - print sign of number
+* @n: number to check
+* Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+*/
 
 
 
