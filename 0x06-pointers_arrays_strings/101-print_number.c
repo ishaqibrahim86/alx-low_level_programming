87caa2b8f118037cd6276b24eae737e9dff1a52a
Ishaq Ibrahim Sadiq <ishaqibrahim86@gmail.com>
2022-09-21 16:05:56 -0700

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,22 +3,13 @@
 
 
 /**
-
 * infinite_add - adds two numbers
-
 * @n1: first number
-
 * @n2: second number
-
 * @r: buffer for result
-
 * @size_r: buffer size
-
 *return: address of r or 0
-
 */
-
-
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
