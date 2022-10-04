d0b67a549958a20bc95ab1d5942dbf99e580e8c5
Ishaq Ibrahim Sadiq <ishaqibrahim86@gmail.com>
2022-10-03 17:57:42 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,33 +16,25 @@ int i, j;
 
 if (width <= 0 || height <= 0)
 {
-
 return (NULL);
 }
 
 grid = malloc(sizeof(int *) * height); /*malloc*/
-
 if (grid == NULL)
 {
-
 return (NULL);
 }
 
 for (i = 0; i < height; i++)
 {
-
 grid[i] = malloc(sizeof(int) * width);
-
 if (grid[i] == NULL)
 {
-
 for (i = i - 1; i >= 0; i--)
 {
-
 free(grid[i]);
 }
 free(grid);
-
 return (NULL);
 }
 }
