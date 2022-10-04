f8aff3710514831581a8293a7d14d31d8ef2c858
Ishaq Ibrahim Sadiq <ishaqibrahim86@gmail.com>
2022-10-03 18:12:55 -0700

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,16 +13,11 @@
 int count_word(char *s)
 
 {
-
 	int flag, c, w;
-
-
 
 	flag = 0;
 
 	w = 0;
-
-
 
 	for (c = 0; s[c] != '\0'; c++)
 
@@ -39,15 +34,10 @@ int count_word(char *s)
 			flag = 1;
 
 			w++;
-
 		}
-
 	}
 
-
-
 	return (w);
-
 }
 
 /**
