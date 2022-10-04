8b3a62daca8d251cfdeee94a44e8967e53239bf4
Ishaq Ibrahim Sadiq <ishaqibrahim86@gmail.com>
2022-10-03 17:11:06 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,78 +1,27 @@
-nclude "main.h"
+include "main.h"
 
 /**
- *
- *  *create_array - array for prints a string
- *
- *   *@size: number elements array
- *
- *    *@c: char
- *
- *     *Return: pointer
- *
- *      */
-
-
+*create_array - array for prints a string
+*@size: number elements array
+*@c: char
+*Return: pointer
+*/
 
 char *create_array(unsigned int size, char c)
-
 {
-
-	char *buffer;
-
-	unsigned int position;
-
-
+	char *array;
+	unsigned int index;
 
 	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof (char) * size);
 
-	{
-
+	if (array == NULL)
 		return (NULL);
 
-	}
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-
-
-	/*Define values with malloc*/
-
-	buffer = (char *) malloc(size * sizeof(c));
-
-
-
-	if (buffer == 0)
-
-	{
-
-		return (NULL);
-
-	}
-
-
-
-	else
-
-	{
-
-		position = 0;
-
-		while (position < size) /*While for array*/
-
-		{
-
-			*(buffer + position) = c;
-
-			position++;
-
-		}
-
-
-
-		return (buffer);
-
-	}
-
-
-
+	return (array);
 }
 
