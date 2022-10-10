698baed51b1fa62cbc9f72ac62895718c8aaad87
Ishaq Ibrahim Sadiq <ishaqibrahim86@gmail.com>
2022-10-10 16:54:08 -0700

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
 /**
-
- * main - prints the name of the file program was compiled from
-
- * Return: Always 0
-
- */
-
-
+* main - prints the name of the file program was compiled from
+* Return: Always 0
+*/
 
 int main(void)
 
